37ffabaf3d285ab11b8d093f6d07d137606f3899
snarky <snarky@mail.thesnarky.com>
2010-02-19 18:59:36 -0500

    Successful hello world in scheme and ncurses

--- scheme/ncurses.scm
+++ scheme/ncurses.scm
@@ -1,6 +1,19 @@
-(c-define "#include <ncurses.h>")
+(c-declare "#include <ncurses.h>")
 
 
 (define hello-world
-    (lambda ()
-        (print "Hello World\n")))
+    (c-lambda () int "initscr(); printw(\"Hello World\"); refresh(); getch(); endwin();" ))
+
+(hello-world)
+
+;;#include <ncurses.h>
+;;
+;;int main()
+;;{   
+;;    initscr();          /* Start curses mode          */
+;;    printw("Hello World !!!");  /* Print Hello World          */
+;;    refresh();          /* Print it on to the real screen */
+;;    getch();            /* Wait for user input */
+;;    endwin();           /* End curses mode        */
+;;    return 0;
+;;}
