522ac0269d50d366f2d8755220ae2e8267b2e310
snarky <snarky@mail.thesnarky.com>
2010-02-24 21:51:35 -0500

    Working example of splitting my code up into handy includes

--- scheme/library/library.scm
+++ scheme/library/library.scm
@@ -1,3 +1,28 @@
-(define test-func
-    (lambda ()
-        (print "Hello World from test-lib!")))
+(define-syntax define-object
+  (syntax-rules ()
+    [(_ (name . varlist)
+        ((var1 val1) ...)
+        ((var2 val2) ...))
+     (define name
+       (lambda varlist
+         (let* ([var1 val1] ...)
+           (letrec ([var2 val2] ...)
+             (lambda (msg . args)
+               (case msg
+                 [(var2) (apply var2 args)]
+                 ...
+                 [else
+                  (assertion-violation 'name
+                    "invalid message"
+                    (cons msg args))]))))))]
+    [(_ (name . varlist) ((var2 val2) ...))
+     (define-object (name . varlist)
+                    ()
+                    ((var2 val2) ...))]))
+
+ ; send-message abstracts the act of sending a message from the act
+ ; of applying a procedure and allows the message to be unquoted.
+(define-syntax send-message
+  (syntax-rules ()
+    [(_ obj msg arg ...)
+     (obj 'msg arg ...)]))
--- scheme/library/test.scm
+++ scheme/library/test.scm
@@ -1,1 +1,3 @@
-(test-func)
+(define test-tile (tile-obj "@" 0 0 0 (lambda () #t) (lambda () #t) (lambda () #t) '() '()))
+
+(print (send-message test-tile get-symbol) "\n")
